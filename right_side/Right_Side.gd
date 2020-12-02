f8ddad68c58aff3805c6c102e2c9e766d232cde7
keitht <keitht@iu.edu>
2020-12-02 18:40:42 -0500

    6:40 PM

--- right_side/Right_Side.gd
+++ right_side/Right_Side.gd
@@ -15,5 +15,9 @@ func _on_Right_pressed():
 	get_tree().change_scene("res://back/Back.tscn")
 
 
-func _on_key_pressed():
-	pass # Replace with function body.
+func _on_Vent_pressed():
+	get_tree().change_scene("res://right_side/Vent.tscn")
+
+
+func _on_Ladder_pressed():
+	get_tree().change_scene("res://right_side/Exit.tscn")
---
+++ right_side/Vent.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _ready():
+	pass
+
+
+func _on_Return_pressed():
+	get_tree().change_scene("res://right_side/Right_Side.tscn")
